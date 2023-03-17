716bcc5d81124a4a06c2aaa5a874015eb71c733b
bee-joo <109484728+bee-joo@users.noreply.github.com>
2023-03-17 21:06:29 +0300

    Update middlewares.js

--- middlewares.js
+++ middlewares.js
@@ -2,14 +2,16 @@ import { NotFoundError, BadRequestError } from './errors.js';
 
 export const notFoundHandler = (req, res) => {
   res.status(404).render('error.hbs', {
-    error: NotFoundError
+    error: NotFoundError,
+    loggedIn: req.session.loggedin
   });
 }; // хэндлер для ненайденых маршрутов - отправляем код 404 и страницу с ошибкой
 
 export const errorHandler = (err, req, res, next) => {
   console.error(err.stack);
   res.status(500).render('error.hbs', {
-    error: BadRequestError
+    error: BadRequestError,
+    loggedIn: req.session.loggedin
   });
 }; // хэндлер всех ошибок сайта - показываем пользователю страницу с ошибкой и выводим её в консоль
 
@@ -29,7 +31,8 @@ export const checkAdmin = (req, res, next) => {
       error: {
         title: 'Недоступно',
         text: 'Страница недоступна'
-      }
+      },
+      loggedIn: req.session.loggedin
     });
   }
 }; // middleware для проверки роли админа - если в сессии указано, что пользователь админ - пускаем дальше, иначе страница с ошибкой
